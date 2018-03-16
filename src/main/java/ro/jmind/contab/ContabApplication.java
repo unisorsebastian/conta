6864323307a1cbb2c365936eeef1a2f2dd719eda
Sebastian Unisor <unisorsebastian@gmail.com>
2018-03-17 00:33:26 +0200

    init

---
+++ src/main/java/ro/jmind/contab/ContabApplication.java
@@ -0,0 +1,12 @@
+package ro.jmind.contab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabApplication.class, args);
+	}
+}
